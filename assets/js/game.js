0878ef38b035fa324926e3fb17c4d59776e61d09
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-01-09 23:21:42 -0600

    fixing bug for player-name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,26 @@
+/* GAME FUNCTIONS */
+
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 // Log player name/stats
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -32,15 +52,6 @@ var playerInfo = {
       window.alert("You don't have enough money!");
     }
   }
-};
-
-/* GAME FUNCTIONS */
-
-// function to generate a random numeric value
-var randomNumber = function(min, max) {
-  var value = Math.floor(Math.random() * (max - min + 1) + min);
-
-  return value;
 };
 
 // enemy information
